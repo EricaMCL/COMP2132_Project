16d5bbcd7dce5c7c1feb198011e5ad2e4b55def2
erica <halamay1029@gmail.com>
2024-11-25 16:03:19 +0800

    functions

--- scripts/project.js
+++ scripts/project.js
@@ -12,8 +12,25 @@ let restartGame = false
 let isWin = false
 let hangman_answer = ""
 
+resetAnswerHint()
+resetImage()
+resetLetterBtn()
 
-function get_answer_n_hint() {
+
+btn_new_game.addEventListener("click", function () {
+    entered_letters = []
+    btn_group_letters.innerHTML = ""
+    num_incorrect_guesses = 0
+    gameOver = false
+    correct_guesses = 0
+    resetImage()
+    resetLetterBtn()
+    resetAnswerHint()
+
+})
+
+
+function resetAnswerHint() {
     fetch("../data/data.json")
         .then(function (response) {
             return response.json();
@@ -33,34 +50,19 @@ function get_answer_n_hint() {
         });
 }
 
-get_answer_n_hint()
-
-
-hangman_image.innerHTML = `<a><img id="hangman_image_1" src="../images/hangman_${num_incorrect_guesses}.png" alt="hangman"></a>`
-console.log("image")
-
-//generates the word guessing section with hidden answer
-
-//const hangman_answer_array = hangman_answer.split('')
-//const required_num_correct_guesses = new Set(hangman_answer_array).size
-
-//for (let answer_char of hangman_answer_array) {
-//hangman_word.innerHTML += `<section class="hangman_answer"><h3 class="invisible_answer">${answer_char}</h3></section>`
-//}
-
-
-//generates letter select buttons
-const letters = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m",
-    "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"]
-
-function letter_btn_generator(letter) {
-    return `<button class="btn_letters" id="btn_letter_${letter}" type="button" onclick="btnClicked ('${letter}')">${letter.toUpperCase()}</button>`
+function resetImage() {
+    hangman_image.innerHTML = `<a><img id="hangman_image_1" src="../images/hangman_${num_incorrect_guesses}.png" alt="hangman"></a>`
 }
 
-let letter;
-for (letter of letters) {
-    const strLetter = letter_btn_generator(letter)
-    btn_group_letters.innerHTML += strLetter
+
+function resetLetterBtn() {
+    const letters = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m",
+        "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"]
+    let letter;
+    for (letter of letters) {
+        const strLetter = `<button class="btn_letters" id="btn_letter_${letter}" type="button" onclick="btnClicked ('${letter}')">${letter.toUpperCase()}</button>`
+        btn_group_letters.innerHTML += strLetter
+    }
 }
 
 
@@ -115,22 +117,6 @@ function btnClicked(letterEntered) {
 
 }
 
-    btn_new_game.addEventListener("click", function () {
-        entered_letters = []
-        btn_group_letters.innerHTML = ""
-        num_incorrect_guesses = 0
-        hangman_image.innerHTML = `<a><img id="hangman_image_1" src="../images/hangman_${num_incorrect_guesses}.png" alt="hangman"></a>`
-        gameOver = false
-        correct_guesses = 0
-        let letter;
-        let strLetter;
-        for (letter of letters) {
-            strLetter = letter_btn_generator(letter)
-            btn_group_letters.innerHTML += strLetter
-        }
-        get_answer_n_hint()
-
-    })
 
 
 
